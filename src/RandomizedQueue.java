d7d66f44db5d317c5334dbc0274e4f43b7dd6703
Omar Negron <omarnegron92@hotmail.com>
2020-10-21 16:55:04 -0400

    enqueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,17 +1,23 @@
 import java.util.Iterator;
 
+import jdk.jshell.SourceCodeAnalysis.QualifiedNames;
+
 /**
  * @author omarn
  * A randomized queue is similar to a stack or queue, except that the item removed is chosen uniformly at random among items in the data structure. 
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+	Item[] q;
+	private int front, rear, n;
+	
 	/**
 	 * construct an empty randomized queue
 	 */
 	public RandomizedQueue()
 	{
-		
+		q = (Item[]) new Object[1]; // ugly cast. Generic array creation not allowed in Java
+		front = rear = n = 0;
 	}
 	
 	/**
@@ -19,15 +25,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	 * @return
 	 */
 	public boolean isEmpty() {
-		return false;
-	}
-	
+		return front == rear;
+	}	
 	
 	/**
 	 * @return the number of items on the randomized queue
 	 */
 	public int size() {
-		return 0;
+		return n;
 	}
 	
 	/**
@@ -35,7 +40,25 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	 * @param item
 	 */
 	public void enqueue(Item item) {
-		
+			
+		if(n == q.length) {
+			resize(2 * q.length);
+		}
+
+		q[n++] = item;
+		rear = (rear + 1) % n;
+	}
+	
+	/**
+	 * Create an array of twice the size, and copy items
+	 * @param length
+	 */
+	private void resize(int capacity) {
+		Item[] copy = (Item[]) new Object[capacity];
+		for(int i = 0; i < n; i++) {
+			copy[i] = q[i];
+		}
+		q = copy;
 	}
 	
 	/**
@@ -55,7 +78,16 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		RandomizedQueue<String> q = new RandomizedQueue<String>();
+		
+		q.enqueue("a");
+		q.enqueue("b");
+		q.enqueue("c");
+		q.enqueue("d");
+		System.out.println(q.size());
+		System.out.println(q.front);
+		System.out.println(q.rear);
 
 	}
 
